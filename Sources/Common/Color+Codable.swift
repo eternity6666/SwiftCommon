2d1e91e71d2226c11db55cef463c5d34bc2f9ca7
eternity6666 <eternity6666@qq.com>
2025-04-29 14:53:47 +0800

    feat: CustomFont.swift、Color+Codable.swift

---
+++ Sources/Common/Color+Codable.swift
@@ -0,0 +1,23 @@
+//
+//  Color+Codable.swift
+//  Apple
+//
+//  Created by Y1616 on 2025/4/18.
+//
+
+import SwiftUI
+
+@available(macOS 14.0, *)
+extension Color: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let components = try container.decode([CGFloat].self)
+        self = Color(.sRGB, red: components[0], green: components[1], blue: components[2], opacity: components[3])
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        let resolved = self.resolve(in: .init())
+        try container.encode([resolved.red, resolved.green, resolved.blue, resolved.opacity])
+    }
+}
---
+++ Sources/Font/CustomFont.swift
@@ -0,0 +1,43 @@
+//
+//  CustomFont.swift
+//  Apple
+//
+//  Created by Y1616 on 2025/4/18.
+//
+
+import SwiftUI
+
+@available(macOS 10.15, *)
+public extension Font {
+    static func baoTuXiaoBaiTi(_ size: CGFloat = 24) -> Font {
+        return .custom("baotuxiaobaiti", size: size)
+    }
+
+    static func liuHuanKatongShoushu(_ size: CGFloat = 24) -> Font {
+        return .custom("LiuhuanKatongShoushu", size: size)
+    }
+
+    static func pomo(_ size: CGFloat = 24) -> Font {
+        return .custom("pomo", size: size)
+    }
+
+    static func youSheBiaoTiHei(_ size: CGFloat = 24) -> Font {
+        return .custom("YouSheBiaoTiHei", size: size)
+    }
+
+    static func qTXiaotu(_ size: CGFloat = 24) -> Font {
+        return .custom("QTXiaotu", size: size)
+    }
+
+    static func mengshenHandwrittenSC(_ size: CGFloat = 24) -> Font {
+        return .custom("Mengshen-Handwritten-SC", size: size)
+    }
+
+    static func starLoveSweety(_ size: CGFloat = 24) -> Font {
+        return .custom("StarLoveSweety", size: size)
+    }
+
+    static func sJjnyyjyy(_ size: CGFloat = 24) -> Font {
+        return .custom("SJjnyyjyy", size: size)
+    }
+}
